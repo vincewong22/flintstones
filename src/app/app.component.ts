326acfe97c2daca4f16bd5648b8e7968f5b5fba6
vince <vincewong22@gmail.com>
2017-03-11 15:25:22 -0800

    added table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,17 @@
 import { Component } from '@angular/core';
 import {CartoonCharacter} from './cartoon-character';
-import {DUMMY_DATA} from './data/dummy-data'; 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  characters = DUMMY_DATA;
-  // character: CartoonCharacter = {
-  //   PersonId: 1,
-  //   FirstName: "Fred",
-  //   LastName: "Flintstone",
-  //   Occupation: "Mining Manager",
-  //   Gender: "M",
-  //   Picture:  "http://flintstones.zift.ca/images/flintstone/fred.png",
-  // };
-  selected: CartoonCharacter; 
-  onSelect(character: CartoonCharacter): void {
-    this.selected= character;
-}
+  character: CartoonCharacter = {
+    PersonId: 1,
+    FirstName: "Fred",
+    LastName: "Flintstone",
+    Occupation: "Mining Manager",
+    Gender: "M",
+    Picture:  "http://flintstones.zift.ca/images/flintstone/fred.png",
+  };
 }
