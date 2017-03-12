cd822a9f6db5790466821e8d00e3552aa2ef5312
vince <vincewong22@gmail.com>
2017-03-11 19:46:45 -0800

    dashboard works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
 import { CharacterDetailComponent } from './character-detail/character-detail.component';
 import { CartoonCharacterComponent } from './cartoon-character/cartoon-character.component';
-
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
     CharacterDetailComponent,
     CartoonCharacterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'characters',
+        component: CartoonCharacterComponent
+        
+      },
+      {
+       path: 'dashboard',
+      component: DashboardComponent
+      }
+      
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
 })
 export class AppModule { }
